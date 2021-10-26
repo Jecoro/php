6022477f5e839ef02bdff073b69a79153751cfb9
Jecoro <jecora98@hotmail.com>
2021-10-26 15:34:03 +0200

    Ej2Mejorado

--- EjerciciosAvanzados/EJ2.php
+++ EjerciciosAvanzados/EJ2.php
@@ -2,15 +2,15 @@
 echo "<h1>Pagina de Bienvenida</h1>";
 $hora = date('G'); 
 switch ($hora) {
-   case (($hora >= 6) AND ($hora < 12)):
+   case (($hora >= 8) AND ($hora < 14)):
         $mensaje = "Buenos dias";
     echo $mensaje;
          break;
-   case (($hora >= 12) AND ($hora < 18)):
+   case (($hora >= 14) AND ($hora < 21)):
          $mensaje = "Buenas tardes"; 
     echo $mensaje;
          break;
-   case (($hora >= 0) AND ($hora < 6)):
+   case (($hora >=21) AND ($hora < 24)):
          $mensaje = "Buenas noches"; 
     echo $mensaje;
          break;
