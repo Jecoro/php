18cd09cc548582bd5bc1d01488a1cafbada6aef0
Jecoro <jecora98@hotmail.com>
2021-10-26 15:22:07 +0200

    Ej1AvanzadoDONE

--- EjerciciosAvanzados/Ej1.php
+++ EjerciciosAvanzados/Ej1.php
@@ -1,4 +1,15 @@
-<?php 
-    echo
+<!DOCTYPE html>
+<html>
 
-?>
+	<head>
+	</head>
+	<body>
+	<?php
+		print ('<h1>Conversión Euros Pesetas</h1>');
+		for($i=1;$i<=10;$i++){
+			echo $i.'€= '.($i*166.386)."</br>";
+		}
+	?>
+
+	</body>
+</html>
